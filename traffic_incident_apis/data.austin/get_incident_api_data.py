2167492f74628eef161528176ab19fc88afcdbfc
hsmckee <56883023+hsmckee@users.noreply.github.com>
2020-02-12 21:48:15 -0600

    Fixed Issues 10, 11, and 12

--- traffic_incident_apis/data.austin/get_incident_api_data.py
+++ traffic_incident_apis/data.austin/get_incident_api_data.py
@@ -12,27 +12,34 @@ engine.execute(f"USE {DATABASE}")
 TABLENAME = "incidents"
 engine.execute(f"DROP TABLE IF EXISTS {TABLENAME}")
 
-#Set up the api url and parameters
+# Set up the api url and parameters
 traffic_incidents = []
-r = requests.get("https://data.austintexas.gov/resource/dx9v-zd7x.json", params = {
-                "$limit": 300000,
-                "$offset": 0
-                })
-if r.status_code == 200:     
-        traffic_incidents = [{
-                                    "ReportID": rec["traffic_report_id"],
-                                    "IncidentDesc": rec["issue_reported"],
-                                    "Address": str(rec["address"]).upper(),
-                                    "IncidentDate": rec["traffic_report_status_date_time"]
-        } for rec in r.json()]     
+r = requests.get(
+    "https://data.austintexas.gov/resource/dx9v-zd7x.json",
+    params={"$limit": 300000, "$offset": 0},
+)
+if r.status_code == 200:
+    traffic_incidents = [
+        {
+            "ReportID": rec["traffic_report_id"],
+            "IncidentDesc": rec["issue_reported"],
+            "Address": str(rec["address"]).upper(),
+            "IncidentDate": rec["traffic_report_status_date_time"],
+        }
+        for rec in r.json()
+    ]
 else:
     print("***** Critical Error Please Try again *****")
     print(r.status_code)
 
 
 traffic_incidents = pd.DataFrame(traffic_incidents)
-traffic_incidents["Street"] = traffic_incidents["Address"].str.extract("(LAMAR)", expand = True)
-traffic_incidents["RecordDate"] = traffic_incidents["IncidentDate"].str.extract("(\d+-\d+-\d+)", expand = True)
+traffic_incidents["Street"] = traffic_incidents["Address"].str.extract(
+    "(LAMAR)", expand=True
+)
+traffic_incidents["RecordDate"] = traffic_incidents["IncidentDate"].str.extract(
+    "(\d+-\d+-\d+)", expand=True
+)
 traffic_inc_clean = traffic_incidents[traffic_incidents.Street.notnull()]
 
 pd.DataFrame(traffic_inc_clean).to_sql(name=TABLENAME, con=engine)
